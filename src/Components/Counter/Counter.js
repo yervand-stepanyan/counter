bd6087ca0f291777be5fe5a68caf082298a90967
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2019-12-26 22:07:01 +0400

    Add local storage data saving

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -5,8 +5,10 @@ class Counter extends React.Component {
   constructor(props) {
     super(props);
 
+    const countFromLocalStorage = JSON.parse(localStorage.getItem("count"));
+
     this.state = {
-      count: this.props.initialCount,
+      count: countFromLocalStorage || this.props.initialCount,
     };
   }
 
@@ -27,17 +29,23 @@ class Counter extends React.Component {
   };
 
   render() {
+    localStorage.setItem("count", JSON.stringify(this.state.count));
+
     return (
       <div className="main">
         <div className="countDiv">Count: {this.state.count}</div>
         <div className="buttonsDiv">
           <button className="btnInc" onClick={this.handleAddCount}>+</button>
-          <button className={this.state.count === 0 ? "btnDecDisabled" : "btnDec"}
+          <button className={this.state.count === 0 ? "btnDisabled" : "btnDec"}
                   onClick={this.handleSubtractCount}
                   disabled={this.state.count <= 0}>
             -
           </button>
-          <button className="btnRes" onClick={this.handleResetToZero}>Reset</button>
+          <button className={this.state.count === 0 ? "btnDisabled" : "btnRes"}
+                  onClick={this.handleResetToZero}
+                  disabled={this.state.count <= 0}>
+            Reset
+          </button>
         </div>
       </div>
     )
