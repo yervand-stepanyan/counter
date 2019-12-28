ec1946f343e96d01966a21f3009b4071b350fd83
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2019-12-28 23:04:53 +0400

    Updates

---
+++ precache-manifest.02c06fe7a97faf64590c217ca3d06352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a187a1b235bcc6a43e657b7a8e26bdd",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "7b77c7a4990a988d812e",
+    "url": "/counter/static/css/main.02f1d031.chunk.css"
+  },
+  {
+    "revision": "a4748b710ccd6dc04006",
+    "url": "/counter/static/js/2.c90129f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/counter/static/js/2.c90129f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b77c7a4990a988d812e",
+    "url": "/counter/static/js/main.50bd9fbd.chunk.js"
+  },
+  {
+    "revision": "75b37e61db02094a16b6",
+    "url": "/counter/static/js/runtime-main.5748cf7c.js"
+  }
+]);
